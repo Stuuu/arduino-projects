64320021a5949b2c63f2d968860080a44124dd48
Stuuu <stuartschrick@gmail.com>
2020-11-03 21:34:25 -0500

    Basic Joystick code to serial monitor

---
+++ Joystick/Joystick.ino
@@ -0,0 +1,31 @@
+int Xpin = A0;
+int Ypin = A1;
+int Spin = 2;
+int Xval;
+int Yval;
+int Sval;
+int dt = 400;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(Xpin,INPUT);
+  pinMode(Ypin,INPUT);
+  pinMode(Spin,INPUT);
+
+// setting this high to avoid using pull up resistor
+  digitalWrite(Spin,HIGH);
+}
+
+void loop() {
+  Xval = analogRead(Xpin);
+  Yval = analogRead(Ypin);
+  Sval = digitalRead(Spin);
+  delay(dt);
+  Serial.print(" X Value = ");
+  Serial.print(Xval);
+  Serial.print(" Y Value = ");
+  Serial.print(Yval);
+  Serial.print(" Switch State is ");
+  Serial.println(Sval);
+}
